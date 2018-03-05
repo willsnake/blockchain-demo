105047ff255d29502b5b858aab86923a90be89b3
Daniel Rodríguez Monroy <willsnake87@gmail.com>
2018-03-04 20:52:21 -0600

    WIP
    * Ordered proyect on folders
    * Added express
    * Added Eslint
    * Added prettier

--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -1,4 +1,4 @@
-const Blockchain = require('./blockchain');
+const Blockchain = require('./index');
 const Block = require('./block');
 
 describe('Blockchain', () => {
@@ -32,5 +32,17 @@ describe('Blockchain', () => {
         bc2.addBlock('foo');
         bc2.chain[1].data = 'Not foo';
         expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('replaces the chain with a valid chain', () => {
+        bc2.addBlock('goo');
+        bc.replaceChain(bc2.chain);
+        expect(bc.chain).toEqual(bc2.chain);
+    });
+
+    it('does not replace the chain with one of less than or equal length', () => {
+        bc.addBlock('foo');
+        bc.replaceChain(bc2.chain);
+        expect(bc.chain).not.toEqual(bc2.chain);
     });
 });
